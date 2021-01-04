fbb1b2383560d2d49449db3ea2e96e4ba6d3d10a
Geno Shamugia <48299520+shamo0@users.noreply.github.com>
2021-01-03 23:56:32 -0800

    Update BotClean.py

--- BotClean.py
+++ BotClean.py
@@ -1,23 +1,30 @@
 #!/bin/python3
 '''
-A deterministic environment is one where the next state is completely determined by the current state of the environment and the task executed by the agent. If there is any randomness involved in determining the next state, the environment is stochastic.
+A deterministic environment is one where the next state is completely determined 
+by the current state of the environment and the task executed by the agent. If there 
+is any randomness involved in determining the next state, the environment is stochastic.
 
-The game Bot Clean took place in a deterministic environment. In this version, the bot is given 200 moves to clean as many dirty cells as possible. The grid initially has 1 dirty cell. When the bot cleans this cell, a new cell in the grid is made dirty. The new cell can be anywhere in the grid.
+The game Bot Clean took place in a deterministic environment. In this version, the 
+bot is given 200 moves to clean as many dirty cells as possible. The grid initially 
+has 1 dirty cell. When the bot cleans this cell, a new cell in the grid is made dirty. 
+The new cell can be anywhere in the grid.
 
-The bot here is positioned at the top left corner of a 5*5 grid. Your task is to move the bot to appropriate dirty cell and clean it.
+The bot here is positioned at the top left corner of a 5*5 grid. Your task is to move 
+the bot to appropriate dirty cell and clean it.
 
 Input Format
-The first line contains two single spaced integers which indicates the current position of the bot. The grid is indexed (x, y) 0<=x,y<=4 top to bottom and left to right respectively. Refer to to board convention here.
+The first line contains two single spaced integers which indicates the current position
+of the bot. The grid is indexed (x, y) 0<=x,y<=4 top to bottom and left to right 
+respectively. Refer to to board convention here.
 
 5 lines follow showing the grid rows. Each cell in the grid is represented by any of the following 3 characters:
 
 'b' (ascii value 98) - the bot's current position (if on clean cell).
-
 'd' (ascii value 100) - a dirty cell (even if the robot is present on top of it).
-
 '-' (ascii value 45) - a clean cell in the grid.
-'''
 
+Author: shamo0
+'''
 def nextMove(posr, posc, board):
     location_row = 0
     location_col = 0
@@ -38,8 +45,6 @@ def nextMove(posr, posc, board):
     else:
         print("CLEAN")
         
-    
-    
 if __name__ == "__main__":
     pos = [int(i) for i in input().strip().split()]
     board = [[j for j in input().strip()] for i in range(5)]
